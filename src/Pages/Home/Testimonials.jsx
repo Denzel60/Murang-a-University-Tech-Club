eb3a56aafe682ca478da6c63058231be983de6db
Denzel <lawrencedenzel60@gmail.com>
2024-06-02 13:54:51 +0300

    Added responsiveness to home testimonial section and Track form section

--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -4,28 +4,30 @@ const Testimonials = () => {
     return (
         <section className='testimonials'>
             <div className="alumni">
-            <img src="" alt="Images for John Doe" />
-            <h3>Alumni John Doe</h3>
-            <p className='description'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus officiis fugit qui, provident ipsa molestias sit dolor ad sunt unde earum fugiat minima ipsum, fuga nesciunt amet nobis debitis?
-            </p>
+                <img src="" alt="Images for John Doe" />
+                <h3>Alumni John Doe</h3>
+                <p className='description'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus officiis fugit qui, provident ipsa molestias sit dolor ad sunt unde earum fugiat minima ipsum, fuga nesciunt amet nobis debitis?
+                </p>
             </div>
+
             <div className="alumni">
-            <img src="" alt="Images for John Doe" />
-            <h3>Alumni John Doe</h3>
-            <p className='description'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus officiis fugit qui, provident ipsa molestias sit dolor ad sunt unde earum fugiat minima ipsum, fuga nesciunt amet nobis debitis?
-            </p>
+                <img src="" alt="Images for John Doe" />
+                <h3>Alumni John Doe</h3>
+                <p className='description'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus officiis fugit qui, provident ipsa molestias sit dolor ad sunt unde earum fugiat minima ipsum, fuga nesciunt amet nobis debitis?
+                </p>
             </div>
+
             <div className="alumni">
-            <img src="" alt="Images for John Doe" />
-            <h3>Alumni John Doe</h3>
-            <p className='description'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus officiis fugit qui, provident ipsa molestias sit dolor ad sunt unde earum fugiat minima ipsum, fuga nesciunt amet nobis debitis?
-            </p>
+                <img src="" alt="Images for John Doe" />
+                <h3>Alumni John Doe</h3>
+                <p className='description'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus officiis fugit qui, provident ipsa molestias sit dolor ad sunt unde earum fugiat minima ipsum, fuga nesciunt amet nobis debitis?
+                </p>
             </div>
         </section>
     );
-  };
-  export default Testimonials;
+};
+export default Testimonials;
 
