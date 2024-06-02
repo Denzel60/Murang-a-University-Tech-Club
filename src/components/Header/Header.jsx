19d2ce24973ea1efd9d87fc8bd7fa13c12aecc73
Denzel <lawrencedenzel60@gmail.com>
2024-06-02 14:04:06 +0300

    Header section made responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,19 @@ import '../../components/Header/Header.css'
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    return(
+    return (
         <div>
             <div className="header">
-            <div className="logo">
-             {/* eslint-disable-next-line react/no-unescaped-entities */}
-            <h2> Murang'a University Tech Club (MUTC)</h2>
-            </div>
-            <div className="header-left">
-                <h3><Link to="/">Home</Link></h3>
-                <h3><Link to="/Leadersip">Leadership</Link></h3>
-                <h3><Link to="/Tracks">Tracks</Link></h3>
-                <h3><Link to="/Events">Events</Link></h3>
-            </div>
+                <div className="logo">
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    <h2> Murang'a University Tech Club (MUTC)</h2>
+                </div>
+                <div className="header-left">
+                    <h3><Link to="/">Home</Link></h3>
+                    <h3><Link to="/Leadersip">Leadership</Link></h3>
+                    <h3><Link to="/Tracks">Tracks</Link></h3>
+                    <h3><Link to="/Events">Events</Link></h3>
+                </div>
             </div>
         </div>
     )
