dbfd53692bb1d8581cf74701cc1a120fad224772
Denzel <lawrencedenzel60@gmail.com>
2024-06-03 15:40:53 +0300

    Altered the header section and made it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import '../../components/Header/Header.css'
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa6";
 
 const Header = () => {
     return (
@@ -9,11 +10,14 @@ const Header = () => {
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
                     <h2> Murang'a University Tech Club (MUTC)</h2>
                 </div>
-                <div className="header-left">
+                <div className="header-left" id='navbar'>
                     <h3><Link to="/">Home</Link></h3>
                     <h3><Link to="/Leadership">Leadership</Link></h3>
                     <h3><Link to="/Tracks">Tracks</Link></h3>
                     <h3><Link to="/Events">Events</Link></h3>
+                </div>
+                <div>
+                    <h3 className='bar' id='bar'><FaBars /></h3>
                 </div>
             </div>
         </div>
